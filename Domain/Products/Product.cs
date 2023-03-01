971dec1baa91a13f80ab914fe24a78c8dc04b249
Diego <diego_sousa.s@outlook.com>
2023-03-01 09:35:52 -0300

    Contexto do EFCore

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -4,6 +4,7 @@ public class Product : BaseEntity
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
     public bool HasStock { get; set; }
 }
