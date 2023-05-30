e45a3be8d2dbc4f138f11aab4988840873ca4b28
Diego <diego_sousa.s@outlook.com>
2023-05-29 21:44:54 -0300

    Utilizando Dapper para realizar consultas

--- Endpoints/Employees/EmployeeGetAll.cs
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 
 namespace IWantApp.Endpoints.Employees;
 
@@ -8,19 +10,23 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handler => Action;
 
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int? page, int? rows, IConfiguration configuration)
     {
-        var users = userManager.Users
-            .Skip((page - 1) * rows)
-            .Take(rows).ToList();
-        var employees = new List<EmployeeResponse>();
+        if (page == null)
+            return Results.BadRequest("Page is required");
 
-        users.ForEach(u =>
-        {
-            var claims = userManager.GetClaimsAsync(u).Result;
-            var username = claims.FirstOrDefault(c => c.Type == "Name");
-            employees.Add(new EmployeeResponse(u.Email, username != null ? username.Value : ""));
-        });
+        if (rows == null)
+            return Results.BadRequest("Rows is required");
+
+        var db = new SqlConnection(configuration.GetConnectionString("SqlServer"));
+        var employees = db.Query<EmployeeResponse>(
+            @"SELECT EMAIL, CLAIMVALUE AS NAME 
+            FROM AspNetUsers A 
+            INNER JOIN AspNetUserClaims B
+            ON A.ID = B.USERID AND B.CLAIMTYPE = 'NAME' 
+            ORDER BY NAME
+            OFFSET (@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY", 
+            new { page, rows } );
 
         return Results.Ok(employees);
     }
