902e16d7b5a25577fe2e7f500bc31053591f0c14
Diego <diego_sousa.s@outlook.com>
2023-05-30 22:50:50 -0300

    Utilizando os parâmetros do token do arquivos de configurações

--- Endpoints/Security/TokenPost.cs
+++ Endpoints/Security/TokenPost.cs
@@ -13,13 +13,13 @@ public class TokenPost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handler => Action;
 
-    public static IResult Action(LoginRequest loginRequest, UserManager<IdentityUser> userManager)
+    public static IResult Action(LoginRequest loginRequest, IConfiguration configuration, UserManager<IdentityUser> userManager)
     {
         var user = userManager.FindByEmailAsync(loginRequest.Email).Result;
         if (user == null || !userManager.CheckPasswordAsync(user, loginRequest.Password).Result)
             return Results.BadRequest();
 
-        var key = Encoding.ASCII.GetBytes("ashduas1h231923@#@#!h7sadhasud");
+        var key = Encoding.ASCII.GetBytes(configuration["JwtBearerTokenSettings:SecretKey"]);
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(new Claim[]
@@ -27,8 +27,8 @@ public class TokenPost
                 new Claim(ClaimTypes.Email, loginRequest.Email),
             }),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-            Audience = "IWantApp",
-            Issuer = "Issuer"
+            Audience = configuration["JwtBearerTokenSettings:Audience"],
+            Issuer = configuration["JwtBearerTokenSettings:Issuer"]
         };
 
         var tokenHandler = new JwtSecurityTokenHandler();
