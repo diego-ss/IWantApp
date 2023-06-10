c6d6bd0e00543bca3fee8efcd1ec390cfe80d2f3
Diego <diego_sousa.s@outlook.com>
2023-06-10 17:05:24 -0300

    Convertendo EmployeePost para async

--- Endpoints/Employees/EmployeePost.cs
+++ Endpoints/Employees/EmployeePost.cs
@@ -1,5 +1,4 @@
 ﻿using IWantApp.Endpoints.Extensions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 
@@ -11,25 +10,25 @@ public class EmployeePost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handler => Action;
 
-    public static IResult Action(EmployeeRequest employeeRequest, HttpContext httpContext, UserManager<IdentityUser> userManager)
+    public static async Task<IResult> Action(EmployeeRequest employeeRequest, HttpContext httpContext, UserManager<IdentityUser> userManager)
     {
         // criando novo usuário
         var employee = new IdentityUser();
         employee.Email = employeeRequest.Email;
         employee.UserName = employeeRequest.Name;
-        var result = userManager.CreateAsync(employee, employeeRequest.Password).Result;
+        var result = await userManager.CreateAsync(employee, employeeRequest.Password);
         var userId = httpContext.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
 
 
         if (!result.Succeeded)
             return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());
 
-        var claimResult = userManager.AddClaimsAsync(employee,
+        var claimResult = await userManager.AddClaimsAsync(employee,
             new Claim[] {
                 new Claim("EmployeeCode", employeeRequest.EmployeeCode),
                 new Claim("Name", employeeRequest.Name),
                 new Claim("CreateBy", userId)
-            }).Result;
+            });
 
         if (!claimResult.Succeeded)
             return Results.BadRequest(claimResult.Errors);
