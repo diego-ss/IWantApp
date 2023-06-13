9f5bb904a59114e5763e4e900abde1e3c4d72e53
Diego <diego_sousa.s@outlook.com>
2023-06-13 19:28:08 -0300

    Paginação dos produtos

--- Endpoints/Products/ProductGetShowcase.cs
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -10,11 +10,28 @@ public class ProductGetShowcase
     public static Delegate Handler => Action;
 
     [AllowAnonymous]
-    public static async Task<IResult> Action(ApplicationDbContext context)
+    public static async Task<IResult> Action(int? page, int? rows, string? orderBy, ApplicationDbContext context)
     {
-        var products = context.Products.Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name).ToList();
+        if (page == null)
+            page = 1;
+
+        if (rows == null)
+            rows = 10;
+
+        if (string.IsNullOrEmpty(orderBy))
+            orderBy = "name";
+
+        var queryBase = context.Products.Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.Active);
+
+        if (orderBy == "name")
+            queryBase = queryBase.OrderBy(p => p.Name);
+        else
+            queryBase = queryBase.OrderBy(p => p.Price);
+
+        var queryFilter = queryBase.Skip((page.Value - 1) * rows.Value).Take(rows.Value);
+  
+        var products = queryFilter.OrderBy(p => p.Name).ToList();
 
         var results = products.Select(p => new ProductsResponse(p.Name, p.Category.Name, p.Description, p.Price, p.HasStock, p.Active));
         return Results.Ok(results);
